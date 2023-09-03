4ed96b1f736725d58d5cfc94c314fc4729ac8406
Anara Sagyndykova <107611667+AnaraSaga@users.noreply.github.com>
2023-09-03 11:53:39 +0600

    table in My Onboard created

--- src/main/java/com/company/onboarding3008/screen/myonboard/MyOnboardScreen.java
+++ src/main/java/com/company/onboarding3008/screen/myonboard/MyOnboardScreen.java
@@ -1,10 +1,27 @@
 package com.company.onboarding3008.screen.myonboard;
 
+import com.company.onboarding3008.entity.User;
+import com.company.onboarding3008.entity.UserStep;
+import io.jmix.core.security.CurrentAuthentication;
+import io.jmix.ui.model.CollectionLoader;
 import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.Subscribe;
 import io.jmix.ui.screen.UiController;
 import io.jmix.ui.screen.UiDescriptor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("MyOnboardScreen")
 @UiDescriptor("my-onboard-screen.xml")
 public class MyOnboardScreen extends Screen {
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
+    @Autowired
+    private CollectionLoader<UserStep> userStepsDl;
+
+    @Subscribe
+    public void onBeforeShow(final BeforeShowEvent event) {
+        final User user = (User) currentAuthentication.getUser();
+        userStepsDl.setParameter("user", user);
+        userStepsDl.load();
+    }
 }
