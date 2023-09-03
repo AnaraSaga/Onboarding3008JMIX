c23a4d59bc9bc782d44ff87ad72d6bdbb142384d
Anara Sagyndykova <107611667+AnaraSaga@users.noreply.github.com>
2023-09-03 11:24:55 +0600

    screen My onboard added

---
+++ src/main/java/com/company/onboarding3008/screen/myonboard/MyOnboardScreen.java
@@ -0,0 +1,10 @@
+package com.company.onboarding3008.screen.myonboard;
+
+import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("MyOnboardScreen")
+@UiDescriptor("my-onboard-screen.xml")
+public class MyOnboardScreen extends Screen {
+}
